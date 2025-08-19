e60b1cc1b3a372e3ec0cdfdb01575a913f367901
Sameer Edlabadkar <sameer.edlabadkar@datamato.com>
2025-08-19 17:02:24 +0530

    adding files

--- backend/server.js
+++ backend/server.js
@@ -15,4 +15,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/activity", activityRoutes);
 
-app.listen(5000, () => console.log("🚀 Server running at http://localhost:5000"));
+
+
+export default app;
